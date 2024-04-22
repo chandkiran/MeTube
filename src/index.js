20800da1c0debd844103b7e4d2d4088bf63c4f21
chandkiran <kiran.772419@thc.tu.edu.np>
2024-04-22 20:24:34 +0545

    Utils added

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ import dotenv from "dotenv";
 dotenv.config({
   path: "./env"
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port: ${process.env.PORT}`);
+    });
+  })
+
+  .catch((error) => {
+    console.log("MOngodb connection failed", error);
+  });
 
 // require("dotenv").config();
 
